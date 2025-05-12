1897d22475871f2f8685c67b54dcfba09ece13b2
Corentin Crepin <117720106+crepincorentin@users.noreply.github.com>
2025-05-12 14:32:14 +0200

    feat : build calculator UI with display, buttons, and history using Tailwind

---
+++ src/components/Display.tsx
@@ -0,0 +1,10 @@
+type Props = {
+    value: string;
+};
+
+export const Display = ({ value }: Props) => (
+    <div className="text-white text-right text-5xl py-6 px-4 font-semibold w-full">
+      {value}
+    </div>
+  );
+  
