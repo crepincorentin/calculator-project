d91da2d6ab7f056ca36c85f78d3c87ed5e72516b
Corentin Crepin <117720106+crepincorentin@users.noreply.github.com>
2025-05-12 11:10:53 +0200

    test : add tests for subtraction and multiplication

--- src/tests/calculate.test.ts
+++ src/tests/calculate.test.ts
@@ -2,8 +2,18 @@ import { describe, it, expect } from 'vitest';
 import { calculate } from '../utils/calculate';
 
 describe('calculate()', () => {
-  it('should return 5 when adding 2 and 3', () => {
-    const result = calculate(2, 3, '+');
-    expect(result).toBe(5);
-  });
+    it('should return 5 when adding 2 and 3', () => {
+        const result = calculate(2, 3, '+');
+        expect(result).toBe(5);
+    });
+
+    it('should return -1 when subtracting 2 and 3', () => {
+        const result = calculate(2, 3, '-');
+        expect(result).toBe(-1);
+    });
+  
+    it('should return 6 when multiplying 2 and 3', () => {
+        const result = calculate(2, 3, '*');
+        expect(result).toBe(6);
+    });
 });
